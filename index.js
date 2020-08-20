ffe8b8a9b41f92a4fbbd030be7ab25757c7f5808
Moch Rizal Rachmadani <mochrira@gmail.com>
2020-08-20 15:50:28 +0700

    fixing build with complete tasks

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var fs = require('fs');
 
 exports.setup = function(srcDir, outDir) {
-    
-    gulp.task('build', function ()  {
+
+    gulp.task('clean', function () {
+        var isExists = fs.existsSync(outDir);
+        if(isExists) {
+            return gulp.src(outDir).pipe(clean({force: true, allowEmpty: true}));
+        }
+        return Promise.resolve(true);
+    });
+
+    gulp.task('source', function () {
+        return gulp.src(srcDir + '/**/*.!(map|scss)')
+            .pipe(gulp.dest(outDir));
+    });    
+
+    gulp.task('sass', function () {
         return gulp.src(srcDir + '/**/*.scss')
             .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest(outDir))
+            .pipe(gulp.dest(outDir));
     });
+
+    gulp.task('build', gulp.series(['clean', 'source', 'sass']));
 
 }
